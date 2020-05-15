2142390513be04a0f0140cb2a17c1bb7d2225c27
Armen G <gharibi.armen@yahoo.com>
2020-05-15 14:56:22 -0700

    Added custom remember me

--- src/main/java/com/agharibi/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/agharibi/springsecurity/security/ApplicationSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.agharibi.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -35,7 +37,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             .anyRequest()
             .authenticated()
             .and()
-            .httpBasic();
+            .formLogin()
+            .loginPage("/login").permitAll()
+            .defaultSuccessUrl("/courses", true)
+            .and()
+            .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+            .key("somethingVerySecured");
     }
 
     @Bean
