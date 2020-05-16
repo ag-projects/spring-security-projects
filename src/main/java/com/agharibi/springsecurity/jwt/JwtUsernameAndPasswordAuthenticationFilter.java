d176c71ed3c5d5e038b0a838eadebc98b7600b98
Armen G <gharibi.armen@yahoo.com>
2020-05-15 17:35:40 -0700

    JWT token verification and impl logic

---
+++ src/main/java/com/agharibi/springsecurity/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.agharibi.springsecurity.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+
+        try {
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+                .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                                                                                    authenticationRequest.getUsername());
+            // ensures the credentials are valid.
+            Authentication authenticated = authenticationManager.authenticate(authentication);
+
+            return authenticated;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        String secureKey = "securesecuresecuresecuresecuresecuresecuresecuresecuresecuresecuresecuresecuresecuresecure";
+
+        String token = Jwts.builder()
+            .setSubject(authResult.getName())
+            .claim("authorities", authResult.getAuthorities())
+            .setIssuedAt(new Date())
+            .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+            .signWith(Keys.hmacShaKeyFor(secureKey.getBytes()))
+            .compact();
+
+        response.addHeader("Authorization", "Bearer" + token);
+    }
+}
--- src/main/java/com/agharibi/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/agharibi/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.agharibi.springsecurity.security;
 
 import com.agharibi.springsecurity.auth.ApplicationUserService;
+import com.agharibi.springsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +11,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.concurrent.TimeUnit;
-
-import static com.agharibi.springsecurity.security.ApplicationUserRole.*;
+import static com.agharibi.springsecurity.security.ApplicationUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
@@ -34,26 +34,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
-//            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
             .authorizeRequests()
+
             .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
             .antMatchers("/api/**").hasRole(STUDENT.name())
             .anyRequest()
-            .authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login").permitAll()
-            .defaultSuccessUrl("/courses", true)
-            .and()
-            .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-            .key("somethingVerySecured")
-            .and()
-            .logout()
-                .logoutUrl("/logout")
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("XSRF-TOKEN", "JSESSIONID")
-                .logoutSuccessUrl("/login");
+            .authenticated();
+
 
     }
 
